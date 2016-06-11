fbddf0213501871de5231e073cdd9a94782eae38
Lars Ullrich <lullrich.py@gmail.com>
2016-06-11 20:50:23 +0200

    Getting rid of whitespaces like \n and replacing with a single " " (the `data.table` way - I hope)

--- tidy_data.R
+++ tidy_data.R
@@ -5,11 +5,28 @@ library("dplyr")
 library("data.table")
 library("lubridate")
 library("stringr")
-players <- fread("data/player_data.csv", na.strings=c("NA", "-"))
 
+# Loading the data from disk passing a custom NA string, 
+# because some missing values were written in the table as "-"
+players <- fread("data/player_data.csv", na.strings=c("NA", "-")) 
+
+# This gets rid of all whitespaces and replaces them
+# with a single whitespace. This is mostly cosmetics 
+# and makes printing look nicer.
+for(cname in colnames(players)) {
+  set(players, j = cname, value = str_replace_all(players[[cname]], "\\s+", " "))
+}
+
+
+# 
 players <- players %>% 
-  apply(2, str_replace_all, "\\s+", " ") %>% #replace whitespaces with single ws
+  apply(2, str_replace_all, "\\s+", " ") #replace whitespaces with single ws
   mutate(height = extract_numeric(height)) %>% 
-  mutate(weight = extract_numeric(weight))
+  mutate(weight = extract_numeric(weight)) %>% 
+  seperate(date_of_birth__age_, c("date_of_birth", "age"), sep = " ") %>% 
+  mutate(age = extract_numeric(age)) %>% 
+  separate(club, c("club", "league"), sep = " ") %>% 
+  separate(senior_debut, c("senior_debut_date", "senior_debut_match"), sep = ": ") %>% 
+  separate(last_appearance, c("last_appearance_date", "last_appearance_match"), sep = ": ")
 
 players

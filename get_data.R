e464eeb1324c431e8c2390f8405e37d08e48c141
Lars Ullrich <lullrich.py@gmail.com>
2016-06-11 11:56:04 +0200

    working now

--- get_data.R
+++ get_data.R
@@ -1,18 +1,15 @@
-setwd("~/Workspace/R")
+setwd("~/Workspace/R/uefa2016")
 options(stringsAsFactors = F)
+
 library("rvest")
 library("stringr")
 library("lubridate")
 library("data.table")
-start_url <- "http://www.uefa.com/uefaeuro/"
-
-teams <- read_html(start_url) %>% 
-  html_nodes("a.table_team-name_block") %>% 
-  html_attr("href") %>% 
-  unique() %>% 
-  sapply(url_absolute, start_url) 
 
 get_players_urls <- function(url) {
+  # function to get the URL for each player on a team.
+  # Expects a url to a team page.
+  # Returns a vector of URLs.
   res <- read_html(url) %>% 
     html_nodes("a.squad--player-img") %>% 
     html_attr("href") %>% 
@@ -25,65 +22,72 @@ get_player_data <- function(nodes) {
   # Returns a data.frame of the player.
   
   
-  # The UEFA-ID of the team and the player we can get from the links.
-  # The last two links 
-  ids <- nodes %>% 
-    html_nodes("a.navbar-lv3-item-link") %>% 
+  # We get the UEFA-ID of the team and the player from the 
+  # URLs in the 3rd-level navigation links.
+  id <- nodes %>%  
+    html_nodes("li.navbar-lv3-item:nth-child(3) > a") %>% 
     html_attr("href") %>% 
-    str_extract_all("(player=)(\\d+)", simplify = TRUE)
+    str_extract_all("player=\\d+") %>% 
+    str_extract("\\d+")
+
+  team_id <- nodes %>% 
+    html_nodes("li.navbar-lv3-item:nth-child(2) > a") %>% 
+    html_attr("href") %>% 
+    str_extract("team=\\d+") %>% 
+    str_extract("\\d+")
   
   # For some reason the little info at the top of the page
   # has a more detailed position than the main table. So 
   # we get it from there.
   detailed_position <- nodes %>% 
-    html_nodes("span.player-category") %>% 
-    html_text(trim = TRUE) %>% 
-    unique()
+    html_nodes("div.player-header_player-rank > span.player-category") %>% 
+    html_text(trim = TRUE)
   
   # Reads the data from the main table. Sadly, this table isn't 
   # a nicely formatted html table that we could just scrape with html_table().
   # We have to scrape the date and labels separately and put them together later.
-  # We append the id and detailed_position to the data
+  # We add the ids and detailed_position later
   data <- nodes %>% 
     html_nodes("span.profile--list--data") %>% 
-    html_text(trim = TRUE) %>% 
-    append(id) %>% 
-    append(detailed_position)
+    html_text(trim = TRUE) 
   
-  # Here we get the labels from the table and append 
-  # the names for the data that wasn't in the table
+  # Getting the labels
   labels <- nodes %>% 
     html_nodes("span.profile--list--label") %>% 
-    html_text(trim = TRUE) %>% 
-    append("id") %>% 
-    append("detailed_position")
-    
+    html_text(trim = TRUE) 
+  
+  # Labeling the data and turning it into a data.frame
+  # Because it is a named chr-vector, we have to first
+  # turn it into a list.
   names(data) <- labels
-  data.frame(as.list(data))
+  data <- data %>% 
+    as.list() %>% 
+    data.frame()
+  
+  # Adding the additional info 
+  data$id <- id
+  data$team_id <- team_id
+  data$detailed_position <- detailed_position
+  
+  data
 }
 
+
+start_url <- "http://www.uefa.com/uefaeuro/"
 strttme <- now()
-players <- teams %>% 
+# getting the data and making a data.table out of it
+teams <- read_html(start_url) %>% 
+  html_nodes("a.table_team-name_block") %>% 
+  html_attr("href") %>% 
+  unique() %>% 
+  sapply(url_absolute, start_url) %>% 
   lapply(get_players_urls) %>% 
   unlist() %>% 
   lapply(read_html) %>% 
-  lapply(get_player_data)
+  lapply(get_player_data) %>% 
+  rbindlist(fill = TRUE)
 endtme <- now()
-endtme - strttme
-players2 <- rbindlist(players, fill = TRUE)
+total_time <- endtme - strttme
+total_time
 
-test <- read_html("http://www.uefa.com/uefaeuro/season=2016/teams/player=250001968/index.html") %>% html_nodes("a.navbar-lv3-item-link") %>% 
-  html_attr("href") %>% 
-  str_extract_all("(player=|team=)(\\d+)", simplify = TRUE)
-
-test <- read_html("http://www.uefa.com/uefaeuro/season=2016/teams/player=1901409/index.html") %>% get_player_data()
-test[6]
-names(test)
-names(players2)
-test_df <- rbindlist(list(test,test2), fill = TRUE)
-class(test_df)
-
-
-teststr <- "http://www.uefa.com/uefaeuro/season=2016/teams/player=1901409/index.html"
-
-
+write.csv(teams, "data/teams_data.csv")

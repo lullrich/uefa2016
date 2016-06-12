54fc51c688cdadc61b46962ead68c0a935e1d915
Lars Ullrich <lullrich.py@gmail.com>
2016-06-12 23:26:56 +0200

    merging failed, still looking for a key

--- tidy_tm_data.R
+++ tidy_tm_data.R
@@ -5,19 +5,27 @@ library("dplyr")
 library("data.table")
 library("lubridate")
 library("stringr")
-# Loading the data from disk passing a custom NA string, 
-# because some missing values were written in the table as "-"
-tm_players <- fread("data/player_data_tm.csv", na.strings=c("NA", "-")) 
-tm_players <- tm_players %>% 
-  mutate(tm_debut = str_replace(tm_debut, ",", "")) %>% 
-  separate(tm_debut, c("month", "day", "year"), sep = " ") %>% 
-  mutate(month = month_to_num(month)) %>% 
-  gather()
 
 month_to_num <- function(x) {
   # My last attempt at trying to fix the dates... Function from the internet that is really 
   # simple and clever. Just returns the matching subset of a vector 
   # with all the months.
-    c(jan=1,feb=2,mar=3,apr=4,may=5,jun=6,jul=7,aug=8,sep=9,oct=10,nov=11,dec=12)[tolower(x)]
+  c(jan=1,feb=2,mar=3,apr=4,may=5,jun=6,jul=7,aug=8,sep=9,oct=10,nov=11,dec=12)[tolower(x)]
 }
+
+# Loading the data from disk passing a custom NA string, 
+# because some missing values in HTML tables are written as "-".
+# This data is far dirtier than the UEFA data. Then again, the same was 
+# true for the HTML.
+tm_players <- fread("data/player_data_tm.csv", na.strings=c("NA", "-")) 
+
+tm_players <- tm_players %>% 
+  mutate(tm_debut = str_replace(tm_debut, ",", "")) %>% 
+  separate(tm_debut, c("month", "day", "year"), sep = " ") %>% 
+  mutate(month = month_to_num(month)) %>% 
+  unite(debut, year, month, day, sep = "-") %>% 
+  mutate(debut = ymd(debut)) %>% 
+  mutate(tm_height = extract_numeric(tm_height))
+
+write.csv(players, "data/player_data_tm_tidied.csv", row.names = FALSE)
 
